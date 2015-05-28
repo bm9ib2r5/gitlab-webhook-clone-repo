11468aab21d0d546118310b147d6b8cac6aa0e38
bm9ib2r5 <bm9ib2r5>
2015-05-28 14:20:39 +0200

    fix

--- gitlab-webhook-clone-repo.py
+++ gitlab-webhook-clone-repo.py
@@ -17,8 +17,7 @@ sourcedir="/export/www"
 gitserver="git@10.11.11.84:root/"
 
 ############# LOGGING
-log_file = '/tmp/gitlab-webhook-clone-repo.log'
-logging.basicConfig(filename='/tmp/gitlab-webhook-clone-repo.log',level=logging.DEBUG,format='%(thread)d %(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+logging.basicConfig(filename='/tmp/gitlab-webhook-clone-repo.log',level=logging.DEBUG,format='[ %(thread)d ] %(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S ')
 
 ####################
 
@@ -52,7 +51,9 @@ class Handler(BaseHTTPRequestHandler):
 	branch = outb[2]
 	repository=(text['repository']['name'])
 	action=(text['object_kind'])
-	logging.info('BRANCH: '+branch+' REPOSITORY: '+repository+' ACTION: '+action)
+	logging.info(" ")
+	logging.info("====== BRANCH: "+branch+" REPOSITORY: "+repository+" ACTION: "+action+"=====")
+	print(" ")
 	print('===' + ' BRANCH: '+branch+' REPOSITORY: '+repository+' ACTION: '+action)
 
 	repodir=sourcedir+'/'+repository
@@ -66,6 +67,7 @@ class Handler(BaseHTTPRequestHandler):
 		if not os.path.exists(DIR_NAME):
 			gitclone='git clone '+REMOTE_URL+' '+DIR_NAME
 			logging.info('Clonning repository: '+REMOTE_URL+' to '+DIR_NAME)
+			print('Clonning repository: '+REMOTE_URL+' to '+DIR_NAME)
 			print gitclone
 			retvalue=os.system(gitclone)
 			if retvalue != '0':
@@ -77,42 +79,55 @@ class Handler(BaseHTTPRequestHandler):
 			gitcheckout='git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' checkout --quiet --force '+branch
 			githardreset='git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' reset --quiet --hard origin/'+branch
 			gitclean='git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' clean --quiet --force -d -x'
-			print gitsetremote
-			print gitfetch
-			print gitcheckout
-			print githardreset
-			print gitclean
+			#print gitsetremote
+			#print gitfetch
+			#print gitcheckout
+			#print githardreset
+			#print gitclean
 
 			logging.info('Work directory: ' +DIR_NAME)
 			logging.info('Set remote: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' remote set-url origin '+REMOTE_URL )
+			print('Work directory: ' +DIR_NAME)
+			print('Set remote: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' remote set-url origin '+REMOTE_URL )
 			retvalue=os.system(gitsetremote)
 			if retvalue != 0:
 				logging.error("Set remote failed!")
-			print retvalue
+				print("Set remote failed!")
+			#print retvalue
 
 			logging.info('Fetch: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' fetch --quiet --all --prune')
+			print('Fetch: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' fetch --quiet --all --prune')
 			retvalue=os.system(gitfetch)
 			if retvalue != 0:
 				logging.error("Git fetch failed!")
-			print retvalue
+				print("Git fetch failed!")
+			#print retvalue
 
 			logging.info('Checkout: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' checkout --quiet --force '+branch)
+			print('Checkout: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' checkout --quiet --force '+branch)
 			retvalue=os.system(gitcheckout)
 			if retvalue != 0:
 				logging.error("Git checkout failed!")
-			print retvalue
+				print("Git checkout failed!")
+			#print retvalue
 
 			logging.info('Hard reset: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' reset --quiet --hard origin/'+branch)
+			print('Hard reset: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' reset --quiet --hard origin/'+branch)
 			retvalue=os.system(githardreset)
 			if retvalue != 0:
 				logging.error("Git hard reset failed!")
-			print retvalue
+				print("Git hard reset failed!")
+			#print retvalue
 
 			logging.info('Clean: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' clean --quiet --force -d -x')
+			print('Clean: git --git-dir='+DIR_NAME+'/.git --work-tree='+DIR_NAME+' clean --quiet --force -d -x')
 			retvalue=os.system(gitclean)
 			if retvalue != 0:
 				logging.error("Git clean failed!")
-			print retvalue
+				print("Git clean failed!")
+			#print retvalue
+	logging.info(" ")
+	print(" ")
 		
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
